8e6135ab1f028fcc1ad2062bcb736912caca1406
Kaviya Naveen Kumar <kaviya@crystalcodelabs.com>
2024-04-05 19:40:58 +0530

    upload the home screen

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer id="footer">
+   <div class="footer-top">
+   <div class="row">
+            <div class="col-5 ">
+               <div id='logo-info'>
+                  <a href="index.html" class="logo me-auto me-lg-0"><img id='foot-logo' src="https://framerusercontent.com/images/KugEpK91J1wapDo50qvUYgf6xo.svg?scale-down-to=512" alt="" class="img-fluid pb-3"></a>
+                  <div id='foot-org' class="footer-links">
+                        <ul>
+                            <li> <img id='org-name' src="https://framerusercontent.com/images/dBlCO3hLFD1HwSPDxHDDLs9sL3I.svg"></li>
+                            <li> <h4>zionapostolic@gmail.com</h4></li>
+                            <li> <h4>Service Location <br> 8704 Millwoods Road NW <br>Edonton,Alberta, T6K 3J3</h4></li>
+                        </ul>
+                    </div>
+               </div>
+            </div>
+            <div class="col-7 footer-links">
+               <ul id='foot-menu'>
+                  <li>  <a href="#">Home</a></li>
+                  <li>  <a href="#">Bible Study</a></li>
+                  <li>  <a href="#">Sunday Sermons</a></li>
+                  <li>  <a href="#">Prayer & Fasting</a></li>
+                  <li>  <a href="#">Others</a></li>
+               </ul>
+            </div>
+         </div>
+   </div>
+   <div class="copyright">
+        @2024 Zion Apostolic. All Rights Reserved
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,12 @@
+<header id='content'>
+    <div id="left-logo">
+        <img id='logo' src='https://framerusercontent.com/images/KugEpK91J1wapDo50qvUYgf6xo.svg?scale-down-to=512' width="90">
+        <ul id='header-menu'>
+            <li>  <a href="#">Home</a></li>
+            <li>  <a href="#">Bible Study</a></li>
+            <li>  <a href="#">Sunday Sermons</a></li>
+            <li>  <a href="#">Prayer & Fasting</a></li>
+            <li>  <a href="#">Others</a></li>
+        </ul>
+    </div>
+</header>
