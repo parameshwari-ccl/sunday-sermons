9838276278fcfd5d94b4b56534a3f339b76fe096
Parameshwari <ccl.trainee2@gmail.com>
2024-04-22 16:25:41 +0530

    alterations in css

--- search.php
+++ search.php
@@ -7,22 +7,21 @@
   <?php require_once('css.php'); ?>
 </head>
 <body>
-  <!-- header -->
-  <?php require_once('header.php'); ?>
-
-  <!-- background image  -->
-  <div class="img-div">
-    <img class='book-image' width="100%" src='https://framerusercontent.com/images/pH8DlNGW1BQprXwH0NnpNT6R68I.jpg?scale-down-to=4096'>
-  </div>
-
   <!-- main -->
 
-  <a href="/filter-results.php" class="search-btn">
-    <div class="btn-div">
-      <i class="fa fa-search" ></i>
-      <input type="text" placeholder="Find a Sermon">
+  <div class="book-image">
+    <!-- header -->
+    <?php require_once('header.php'); ?>
+
+    <div href="/filter-results.php" class="search-btn">
+      <div class="btn-div">
+          <div class="search-bar">
+            <i class="fa fa-search" ></i>
+            <input type="text" placeholder="Find a Sermon">
+          </div>
+      </div>
     </div>
-  </a>
+  </div>
 
 
   <!-- footer -->
