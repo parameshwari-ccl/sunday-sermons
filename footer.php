3709cad70d4147bd88370f13fde9a1df2e08fe0b
Parameshwari <ccl.trainee2@gmail.com>
2024-04-25 11:57:05 +0530

    footer modification

--- footer.php
+++ footer.php
@@ -1,30 +1,30 @@
 <footer class="footer">
-   <div class="footer-top">
-   <div class="row foot">
-      <div class="col-5 foot-one">
-         <div class='logo-info'>
-            <a href="index.html" class="logo me-auto me-lg-0 "><img class='foot-logo' src="https://framerusercontent.com/images/KugEpK91J1wapDo50qvUYgf6xo.svg?scale-down-to=512" alt="" class="img-fluid pb-3"></a>
-            <div class='foot-org' class="footer-links">
-               <ul>
-                  <li> <img class='org-name' src="https://framerusercontent.com/images/dBlCO3hLFD1HwSPDxHDDLs9sL3I.svg"></li>
-                  <li> <h4>zionapostolic@gmail.com</h4></li>
-                  <li> <h4>Service Location <br> 8704 Millwoods Road NW <br>Edonton,Alberta, T6K 3J3</h4></li>
-               </ul>
+  <div class="foot-wrapper">
+      <div class="row">
+         <div class="col-5 foot-details">
+            <div class='logo-info'>
+               <div class='foot-org' class="footer-links">
+                  <a href="index.html" class="logo me-auto me-lg-0 "><img class='foot-logo' src="https://framerusercontent.com/images/KugEpK91J1wapDo50qvUYgf6xo.svg?scale-down-to=512" alt="" class="img-fluid pb-3"></a>
+                  <ul>
+                     <li> <img class='org-name' src="https://framerusercontent.com/images/dBlCO3hLFD1HwSPDxHDDLs9sL3I.svg"></li>
+                     <li> <h4>zionapostolic@gmail.com</h4></li>
+                     <li> <h4>Service Location <br> 8704 Millwoods Road NW <br>Edonton,Alberta, T6K 3J3</h4></li>
+                  </ul>
+               </div>
             </div>
          </div>
+         <div class="col-7 footer-links">
+            <ul class='foot-menu'>
+               <li>  <a href="index.php">Home</a></li>
+               <li>  <a href="sunday-sermons.php">Sunday Sermons</a></li>
+               <li>  <a href="bible-study.php">Bible Study</a></li>
+               <li>  <a href="prayer-&-fasting.php">Prayer & Fasting</a></li>
+               <li>  <a href="others.php">Others</a></li>
+            </ul>
+         </div>
       </div>
-      <div class="col-7 footer-links foot-two">
-         <ul class='foot-menu'>
-            <li>  <a href="index.php">Home</a></li>
-            <li>  <a href="sunday-sermons.php">Sunday Sermons</a></li>
-            <li>  <a href="bible-study.php">Bible Study</a></li>
-            <li>  <a href="prayer-&-fasting.php">Prayer & Fasting</a></li>
-            <li>  <a href="others.php">Others</a></li>
-         </ul>
+      <div class="copyright">
+         @2024 Zion Apostolic. All Rights Reserved
       </div>
-   </div>
-   </div>
-   <div class="copyright">
-        @2024 Zion Apostolic. All Rights Reserved
-    </div>
+  </div>
 </footer>
