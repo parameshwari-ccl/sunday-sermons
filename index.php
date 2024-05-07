57a755159bf60ea3128ea6111257c27190bb67a4
Parameshwari <ccl.trainee2@gmail.com>
2024-05-07 17:33:34 +0530

    home screen design working

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
                 <source src="video/background.mp4" type="video/mp4">
             </video>
             <!-- main -->
-            <div class='center-content'>
+            <div class='center-content d-flex justify-content-center'>
                 <div class="center-detail">
-                    <h2>Heaven and earth will pass away, but my words will never pass away</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    <h2 class="text-center">Heaven and earth will pass away, but my words will never pass away</h2>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
                         ut aliquip ex ea commodo consequat.
